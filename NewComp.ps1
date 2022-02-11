d3915711b32be2f8eafe9fe26165b3cbf0f0c580
cybrwlf <53710956+cybrwlf@users.noreply.github.com>
2022-02-11 14:58:12 -0500

    Create NewComp.ps1

---
+++ NewComp.ps1
@@ -0,0 +1,42 @@
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+
+# GUI Specs
+Write-Host "Checking winget..."
+
+# Check if winget is installed
+if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
+    'Winget Already Installed'
+}  
+else{
+    # Installing winget from the Microsoft Store
+	Write-Host "Winget not found, installing it now."
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
+	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+	$nid = (Get-Process AppInstaller).Id
+	Wait-Process -Id $nid
+	Write-Host Winget Installed
+    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
+}
+
+$Form                            = New-Object system.Windows.Forms.Form
+$Form.ClientSize                 = New-Object System.Drawing.Point(1050,1000)
+$Form.text                       = "Windows Toolbox By Chris Titus"
+$Form.StartPosition              = "CenterScreen"
+$Form.TopMost                    = $false
+$Form.BackColor                  = [System.Drawing.ColorTranslator]::FromHtml("#e9e9e9")
+$Form.AutoScaleDimensions     = '192, 192'
+$Form.AutoScaleMode           = "Dpi"
+$Form.AutoSize                = $True
+$Form.AutoScroll              = $True
+$Form.ClientSize              = '1050, 1000'
+$Form.FormBorderStyle         = 'FixedSingle'
